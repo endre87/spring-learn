18d88c829bcdb4ed2acb733d9a288b6af29fe982
abiro <Attila-Endre.Biro@frequentis.com>
2021-09-20 09:46:22 +0300

    fixed orderForm

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,58 +1,44 @@
-// tag::baseClass[]
 package tacos.web;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
-import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
-//end::baseClass[]
-//tag::baseClass[]
-
-@Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 
-    //end::baseClass[]
-//tag::orderForm[]
+    private OrderRepository orderRepo;
+
+//    public OrderController(OrderRepository orderRepo) {
+//        this.orderRepo = orderRepo;
+//    }
+
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
-//end::orderForm[]
 
-/*
-    //tag::handlePost[]
     @PostMapping
-    public String processOrder(Order order) {
-        log.info("Order submitted: " + order);
-        return "redirect:/";
-    }
-    //end::handlePost[]
-*/
-
-    //tag::handlePostWithValidation[]
-    @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
+//        orderRepo.save(order);
+        sessionStatus.setComplete();
+
         return "redirect:/";
     }
-//end::handlePostWithValidation[]
-
-//tag::baseClass[]
 
 }
-//end::baseClass[]
